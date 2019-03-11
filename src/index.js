dd10e0842a2c5a8d9fdbbc7ac97694977128db8e
Thor <titaniumwhiskers@gmail.com>
2019-03-11 02:44:45 -0400

    Optimizing memory part 5

--- src/index.js
+++ src/index.js
@@ -8,11 +8,7 @@ import 'semantic-ui-css/semantic.min.css';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
-if (module.hot) {
-    module.hot.accept();
-}
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
