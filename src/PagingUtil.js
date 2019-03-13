1494e93e5f9b878c30b9111aa8c5b406641ddf5d
Thor <titaniumwhiskers@gmail.com>
2019-03-13 02:33:05 -0400

    minor improvement to function readability

--- src/PagingUtil.js
+++ src/PagingUtil.js
@@ -1,9 +1,5 @@
-export const composePage = (dataArray, pageSize, pageNo) => {
-    return (dataArray.slice(pageNo * pageSize, pageNo * pageSize + pageSize));
-};
+export const composePage = (dataArray, pageSize, pageNo) => dataArray.slice(pageNo * pageSize, pageNo * pageSize + pageSize);
 
-export const calcTotalPages = (dataArray, pageSize) => {
-    return Math.ceil(dataArray.length / pageSize);
-};
+export const calcTotalPages = (dataArray, pageSize) => Math.ceil(dataArray.length / pageSize);
 
 
